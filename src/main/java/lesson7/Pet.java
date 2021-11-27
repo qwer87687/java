bc5c22a708567cf24bef2884ad0e265eebed2f9f
qwer87687 <o.two@myrambler.ru>
2021-11-27 13:46:22 +0300

    Работа с классами.

--- src/main/java/lesson7/Pet.java
+++ src/main/java/lesson7/Pet.java
@@ -7,14 +7,6 @@ public class Pet {
     double swimming;
 
 
- /*   public Pet(String name, String colour, double running, double swimming) {
-        this.name = name;
-        this.colour = colour;
-        this.running = running;
-        this.swimming = swimming;
-
-    }
-*/
     public Pet() {
 
     }
